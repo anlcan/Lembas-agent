55ba498553f8fff73d77a56d2afcf1b9abea7e84
anlcan <anilcanbaykal@gmail.com>
2013-06-24 15:57:57 +0300

    added project parameter

--- handsome.py
+++ handsome.py
@@ -1,8 +1,22 @@
 import json
+import argparse
+
 from urllib.request import urlopen
 from os import sep
 from base64 import b64decode
 from os import makedirs
+
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                   help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                   const=sum, default=max,
+                   help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
 
 #HOST="http://sync-server.appspot.com/test"
 HOST="http://moxo.sync-server.appspot.com/"
@@ -15,7 +29,7 @@ PACKAGE="com.nomad.aottservice"
 PLATFORM="java"  # java|android|objc|dotnet
 
 
-url= "%s?target=%s&host=%s&endPoint=%s&port=80&header=no&package=%s" % (HOST,PLATFORM,TARGET, ENDPOINTNAME, PACKAGE)
+url= "%s?target=%s&host=%s&endPoint=%s&port=80&header=no&package=%s&project=%s" % (HOST,PLATFORM,TARGET, ENDPOINTNAME, PACKAGE, ENDPOINTNAME)
 print(url)
 
 f = urlopen(url)
